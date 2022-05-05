74e8d4bf8816176f0a15b677eb826d89b5b64a1b
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-05-05 18:00:54 +0300

    add fonts

--- diagnostics/src/hero/hero.js
+++ diagnostics/src/hero/hero.js
@@ -2,12 +2,22 @@ import React from "react";
 
 import './hero.scss';
 
-import bgimg from './main-image.png';
-
 const Hero = () => {
   return(
     <section className="hero">
-
+      <div className="container">
+        <div className="hero__content">
+          <div className="hero__content-text">
+            <h1 className="hero__title">Независимое обследование зданий и сооружений</h1>
+            <p className="hero__text">антенн, труб, теплосетей, катализаций и других объектов</p>
+          </div>
+          <div className="hero__content-data">
+            <div className="data__text">
+              Отправьте техзадание и получите коммерческое предложение в течении 15 минут на свой email или в мессенджер
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
