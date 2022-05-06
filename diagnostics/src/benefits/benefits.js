85472f8ec9a79ed3914beb76ada6e25943f37b3a
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-05-06 17:20:35 +0300

    benefits desctop done

--- diagnostics/src/benefits/benefits.js
+++ diagnostics/src/benefits/benefits.js
@@ -2,8 +2,48 @@ import React from "react";
 
 import './benefits.scss';
 
-const Benefits = () => {
+import listIcon from './icon.png';
+import tv from './tv.png';
 
+const Benefits = () => {
+  return(
+    <section className="benetits">
+    <img className="device" src={tv} alt="" />
+      <div className="container">
+        <div className="benefits__wrapper">
+          <div className="benetits__about">
+            <h2 className="benefits__title">
+              Мы работаем быстро и наши отчеты проходят согласование с первого раза
+            </h2>
+            <p className="benefits__text">
+              Над вашим объектом работает целая команда: 3 ГИПа (главных инженеров проектов) с опытом работы от 6 лет и 2 штатных сотрудника для камеральных работ
+            </p>
+          </div>
+          <div className="gain">
+            <h3 className="gain__title">Вы и Ваши архитекторы получаете:</h3>
+            <div className="gain__content">
+              <div className="gain__img">
+                <img src={listIcon} alt="" />
+              </div>
+              <div className="gain__txt">Отчет на который на 100% можно положиться </div>
+            </div>
+            <div className="gain__content">
+              <div className="gain__img">
+                <img src={listIcon} alt="" />
+              </div>
+              <div className="gain__txt">Экономию времени и прогнозируемость сроков</div>
+            </div>
+            <div className="gain__content">
+              <div className="gain__img">
+                <img src={listIcon} alt="" />
+              </div>
+              <div className="gain__txt">Предварительные результаты сразу на месте</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Benefits;
